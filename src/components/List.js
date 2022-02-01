ac654b64e9bb1b3e8a6346b809249d5b4c6b8e78
kfirY <81631340+kfiryahav@users.noreply.github.com>
2022-02-01 12:14:53 +0200

    Update List.js
    added ui bold text

--- src/components/List.js
+++ src/components/List.js
@@ -10,9 +10,9 @@ const List = (props) => {
                             <img src={item.webformatURL} className="card-img-top img-responsive rounded shadow" alt={item.tags} />
                         </a>
                         <div className="card-body">
-                            <div className="div">Views: {item.views}</div>
-                            <div className="div">Likes: {item.likes}</div>
-                            <div className="div">Downloads: {item.downloads}</div>
+                            <div><span className="fw-bold">Views: </span>{item.views}</div>
+                            <div><span className="fw-bold">Likes:</span> {item.likes}</div>
+                            <div><span className="fw-bold">Downloads:</span> {item.downloads}</div>
                         </div>
                     </div>
                 )
